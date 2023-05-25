c61de3a4d9d8241a5d356748cbd0ae0eb7b91ea8
Crummorn <igorcaze@gmail.com>
2023-05-25 18:37:04 -0300

    feat(contacts): delete function

--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,6 @@ const router = Router();
 
 router.get('/contacts', ContactControler.index);
 router.get('/contacts/:id', ContactControler.show);
+router.delete('/contacts/:id', ContactControler.delete);
 
 module.exports = router;
